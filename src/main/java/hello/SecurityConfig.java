26c2d8d1114e7782c896348e82e4f2301c758669
Kiran Kumar <kkumar1@paypal.com>
2017-01-05 15:34:43 +0530

    Added security configuration

--- src/main/java/hello/SecurityConfig.java
+++ src/main/java/hello/SecurityConfig.java
@@ -1,30 +1,46 @@
 package hello;
-
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.config.annotation.authentication.configurers.provisioning.UserDetailsManagerConfigurer;
-
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+ 
+@Configuration
 @EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
-
-	@Override
-	protected void configure(HttpSecurity http) throws Exception {
-		http
-			.authorizeRequests()
-					
-				.antMatchers("/indexerre").hasRole("USER")			
-				.and()
-			.formLogin()
-				.loginPage("/login").failureUrl("/login-error");	
-	}
-
-	@Autowired
-	public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
-		UserDetailsManagerConfigurer um=(UserDetailsManagerConfigurer) auth
-			.inMemoryAuthentication();
-		//	um.withUser("user").password("password").roles("USER");
-	}
+ 
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+    	
+    	http.csrf().disable();;
+    	
+    	http
+         .authorizeRequests()
+         .antMatchers("/index**").permitAll()
+         .anyRequest().authenticated()
+         .and()
+         .formLogin()
+             .usernameParameter("j_username")
+             .passwordParameter("j_password")
+             .loginPage("/login")
+             .defaultSuccessUrl("/", true)
+             .permitAll()
+         .and()
+             .logout()
+             .logoutUrl("/logout")
+             .invalidateHttpSession(true)
+             .logoutSuccessUrl("/")
+             .deleteCookies("JSESSIONID")
+             .logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
+         ;
+    }
+  
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+          auth.inMemoryAuthentication().withUser("user").password("password").roles("USER");
+       // auth.userDetailsService(userDetailsService());
+    }
 }
