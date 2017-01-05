9a4770c9e8d93665a920a3a8ef5b2cafdcfabd95
Kiran Kumar <kkumar1@paypal.com>
2017-01-05 15:33:56 +0530

    Added some protected apis

--- src/main/java/hello/HelloController.java
+++ src/main/java/hello/HelloController.java
@@ -1,14 +1,26 @@
 package hello;
 
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @RestController
 public class HelloController {
 
-    @RequestMapping("/index")
+    @RequestMapping(value="/index",method=RequestMethod.GET)
     public String index() {
         return "Greetings from Spring Boot!";
+    }
+    
+    
+    @RequestMapping(value="/pro/id",method=RequestMethod.GET)
+    public String index2() {
+        return "Protected url";
+    }
+    
+    @RequestMapping(value="/",method=RequestMethod.GET)
+    public String hoe() {
+        return "Home!!! protected";
     }
 
 }
